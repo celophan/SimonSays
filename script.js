17da55835027294dd412618a485edea38e2dc6f9
Jean-OIKONOMOU <jean.oikonomou@gmail.com>
2020-04-10 20:01:18 +0200

    images + base styling added

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+I use these :
+    push()
+    querySelector()
+    SetAttribute()
+    getAttribute()
+    appendChild()
+    Math.random()
+    sort()
+    For loops
+    createElement()
+    Sprites taken from https://pokemondb.net/sprites
+*/
+
