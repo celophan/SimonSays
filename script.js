c348010a906bbc948531520eb54f27ca33f1c1c2
Jean-OIKONOMOU <jean.oikonomou@gmail.com>
2020-04-11 20:23:08 +0200

    game almost finished

--- script.js
+++ script.js
@@ -12,3 +12,131 @@ I use these :
     Sprites taken from https://pokemondb.net/sprites
 */
 
+document.addEventListener('DOMContentLoaded', () => {
+
+    // card options
+    const cardArray = [
+        {
+            name: 'blastoise',
+            img: 'img/blastoise.png'
+        },
+        {
+            name: 'charizard',
+            img: 'img/charizard.png'
+        },
+        {
+            name: 'clefable',
+            img: 'img/clefable.png'
+        },
+        {
+            name: 'cloyster',
+            img: 'img/cloyster.png'
+        }, 
+        {
+            name: 'dragonair',
+            img: 'img/dragonair.png'
+        },
+        {
+            name: 'kingler',
+            img: 'img/kingler.png'
+        },
+        {
+            name: 'blastoise',
+            img: 'img/blastoise.png'
+        },
+        {
+            name: 'charizard',
+            img: 'img/charizard.png'
+        },
+        {
+            name: 'clefable',
+            img: 'img/clefable.png'
+        },
+        {
+            name: 'cloyster',
+            img: 'img/cloyster.png'
+        }, 
+        {
+            name: 'dragonair',
+            img: 'img/dragonair.png'
+        },
+        {
+            name: 'kingler',
+            img: 'img/kingler.png'
+        }
+    ]
+
+    cardArray.sort(() => 0.5 - Math.random())
+
+    const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result')
+    const winningTitle = document.querySelector('#title')
+    const resetButton = document.querySelector("#reset")
+    var imgs = document.images;
+    var cardsChosen = []
+    var cardsChosenId = []
+    var cardsWon = []
+
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.setAttribute('src', 'img/pokeball.png')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+        }
+    }
+
+    function checkForMatch() {
+        var cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+        if (cardsChosen[0] === cardsChosen[1]) {
+            // alert('You found a match!')
+            cards[optionOneId].setAttribute('src', 'img/blank.png')
+            cards[optionTwoId].setAttribute('src', 'img/blank.png')
+            cardsWon.push(cardsChosen)
+        } else {            
+            cards[optionOneId].setAttribute('src', 'img/pokeball.png')
+            cards[optionTwoId].setAttribute('src', 'img/pokeball.png')
+            // alert('Nope.')
+        }
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = cardsWon.length
+        if (cardsWon.length === cardArray.length/2) {
+            resultDisplay.textContent = ''
+            winningTitle.textContent = 'Congratulations, you won !'
+        }
+    }
+    
+    function flipCard() {
+        var cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        if(this.className == "flip1")
+            this.className = "flip2";
+        else
+            this.className = "flip1";
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)      
+        }
+    }
+
+    function reset() {
+        cardsChosen = []
+        cardsChosenId = []
+        cardsWon = []
+        cardArray.sort(() => 0.5 - Math.random())
+        for (let i = 0; i < cardArray.length; i++) {
+            var cards = document.querySelectorAll('img')
+            cards[i].setAttribute('src', 'img/pokeball.png')
+        }
+        
+    }
+
+    resetButton.addEventListener('click', reset)
+
+    createBoard()
+})
